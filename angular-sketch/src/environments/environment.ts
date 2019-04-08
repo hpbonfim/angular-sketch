a62c2920e8d435b2968cb194e5e103491337d31e
Henrique <hp_bonfim@hotmail.com>
2019-04-08 06:12:51 -0400

    add firebase config

--- angular-sketch/src/environments/environment.ts
+++ angular-sketch/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDJiYywSDO8TL5Dp8esJrwKDg8bjJUKlOk",
+    authDomain: "angular-sketch-007.firebaseapp.com",
+    databaseURL: "https://angular-sketch-007.firebaseio.com",
+    projectId: "angular-sketch-007",
+    storageBucket: "angular-sketch-007.appspot.com",
+    messagingSenderId: "284236718366"
+  }
 };
 
 /*
